ea6bab58ada93b79044bdb58d35e174b9bf0dfcd
Brub4838ispro <105958283+Bruh4838pc@users.noreply.github.com>
2022-06-10 15:13:15 +0000

    Update start.bat

--- .github/workflows/start.bat
+++ .github/workflows/start.bat
@@ -1,9 +1,9 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By GetDrive" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By Brub4838" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator @GetdDrive!$ /add >nul
+net user administrator Bruh4838 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -17,6 +17,6 @@ echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get a troop tunnel, make sure ngrok_auth_token is correct in Settings> Secrets> Repository Secret.Maybe your previous VM is still running:
 https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: @GetdDrive!$
+echo Password: Bruh4838
 echo Please log in to your RDP!
 ping -n 10 127.0.0.1 >nul
